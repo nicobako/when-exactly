2ab6f3774065bd0056b4a1cf2dddf62dab566ea7
nicobako <nicobako@gmail.com>
2025-01-22 12:46:58 -0600

    refactored

--- tests/test_hour.py
+++ tests/test_hour.py
@@ -18,3 +18,9 @@ def test_hour_minutes() -> None:
         assert minute == we.minute(2020, 1, 1, 0, i)
     assert minutes[-1].start == we.Moment(2020, 1, 1, 0, 59, 0)
     assert minutes[-1].stop == we.Moment(2020, 1, 1, 1, 0, 0)
+
+
+def test_next_hour() -> None:
+    hour = we.hour(2020, 1, 1, 0)
+    assert next(hour) == we.hour(2020, 1, 1, 1)
+    assert next(next(hour)) == we.hour(2020, 1, 1, 2)
--- tests/test_second.py
+++ tests/test_second.py
@@ -20,3 +20,9 @@ def test_assert_second_minute() -> None:
 
 def test_second_end_of_minute() -> None:
     second = we.second(2020, 1, 1, 0, 0, 59)
+    assert second.stop == we.Moment(2020, 1, 1, 0, 1, 0)
+
+
+def test_second_next() -> None:
+    second = we.second(2020, 1, 1, 0, 0, 0)
+    assert next(second) == we.second(2020, 1, 1, 0, 0, 1)
