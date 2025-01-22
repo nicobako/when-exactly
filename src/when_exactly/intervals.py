4cfef1b19274057d222e076d32fdb4ce28fa6d79
nicobako <nicobako@gmail.com>
2025-01-22 12:46:58 -0600

    added more useful methods to Intervals

--- src/when_exactly/intervals.py
+++ src/when_exactly/intervals.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Generic, Iterable, TypeVar
+from typing import Iterable, NoReturn, TypeVar, overload
 
 from when_exactly.interval import Interval
 
@@ -9,7 +9,7 @@ T = TypeVar("T", bound=Interval)
 from collections import abc
 
 
-class Intervals(abc.Iterable[T], Generic[T]):
+class Intervals(abc.Container[T]):
     def __init__(self, values: Iterable[T]) -> None:
         self._values = list(values)
         self._counter = 0
@@ -20,6 +20,34 @@ class Intervals(abc.Iterable[T], Generic[T]):
     def __next__(self) -> T:
         try:
             value = self._values[self._counter]
+            self._counter += 1
             return value
         except IndexError:
+            self._counter = 0
             raise StopIteration
+
+    def __contains__(self, x: object) -> bool:
+        return self._values.__contains__(x)
+
+    @overload
+    def __getitem__(self, index: int) -> T: ...
+
+    @overload
+    def __getitem__(self, index: slice[int]) -> Intervals[T]: ...
+
+    def __getitem__(self, index: int | slice[int]) -> T | Intervals[T]:
+        if isinstance(index, slice):
+            return Intervals(self._values[index])
+        else:
+            return self._values[index]
+
+    def __repr__(self) -> str:
+        return repr(self._values)
+
+    def __eq__(self, other: Intervals[T] | object) -> bool:
+        if not isinstance(other, Intervals):
+            return NotImplemented
+        return self._values == other._values
+
+    def __reversed__(self) -> NoReturn:
+        raise NotImplementedError
--- tests/test_intervals.py
+++ tests/test_intervals.py
@@ -1,14 +1,33 @@
-from assert_frozen import assert_frozen
+import pytest
 
 import when_exactly as we
 
 
-def test_intervals() -> None:
-    values = [
+@pytest.fixture  # type: ignore
+def values() -> list[we.Interval]:
+    return [
         we.Interval(we.Moment(2020, 1, 1, 0, 0, 0), we.Moment(2020, 1, 2, 0, 0, 0)),
         we.Interval(we.Moment(2020, 1, 2, 0, 0, 0), we.Moment(2020, 1, 3, 0, 0, 0)),
         we.Interval(we.Moment(2020, 1, 3, 0, 0, 0), we.Moment(2020, 1, 4, 0, 0, 0)),
     ]
-    intervals = we.Intervals(values)
-    assert_frozen(intervals)
+
+
+@pytest.fixture  # type: ignore
+def intervals(values: list[we.Interval]) -> we.Intervals[we.Interval]:
+    return we.Intervals(values)
+
+
+def test_intervals_collection_api(
+    values: list[we.Interval], intervals: we.Intervals[we.Interval]
+) -> None:
     assert list(intervals) == values
+    assert list(intervals) == values
+
+    assert values[0] in intervals
+    assert intervals[0] == values[0]
+
+    assert intervals[0:2] == we.Intervals(values[0:2])
+    assert intervals == intervals
+
+    with pytest.raises(NotImplementedError):
+        assert reversed(intervals)
