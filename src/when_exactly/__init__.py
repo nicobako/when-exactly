64b3a1c498173662b40c419895352ff47e6da021
nicobako <nicobako@gmail.com>
2025-01-22 12:46:58 -0600

    added basic types

--- src/when_exactly/__init__.py
+++ src/when_exactly/__init__.py
@@ -1,8 +1,21 @@
 from when_exactly.delta import Delta
-from when_exactly.factories import Hour, Minute, Second, hour, minute, now, second
+from when_exactly.factories import (
+    Day,
+    Hour,
+    Minute,
+    Month,
+    Second,
+    Year,
+    day,
+    hour,
+    minute,
+    month,
+    now,
+    second,
+    year,
+)
 from when_exactly.interval import Interval
 from when_exactly.moment import Moment
-from when_exactly.year import Year, year
 
 __all__ = [
     "Delta",
@@ -15,6 +28,12 @@ __all__ = [
     "Minute",
     "hour",
     "Hour",
+    "year",
+    "Year",
+    "day",
+    "Day",
+    "month",
+    "Month",
     "year",
     "Year",
 ]
--- src/when_exactly/factories.py
+++ src/when_exactly/factories.py
@@ -16,11 +16,11 @@ def now() -> Moment:
 def second(
     year: int, month: int, day: int, hour: int, minute: int, second: int
 ) -> Second:
-    start = Moment(year, month, day, hour, minute, second).to_datetime()
-    stop = start + datetime.timedelta(seconds=1)
+    start = Moment(year, month, day, hour, minute, second)
+    stop = start + Delta(seconds=1)
     return Second(
-        start=Moment.from_datetime(start),
-        stop=Moment.from_datetime(stop),
+        start=start,
+        stop=stop,
     )
 
 
@@ -29,10 +29,10 @@ _second = second
 
 def minute(year: int, month: int, day: int, hour: int, minute: int) -> Minute:
     start = Moment(year, month, day, hour, minute, 0)
-    stop = start.to_datetime() + datetime.timedelta(minutes=1)
+    stop = start + Delta(minutes=1)
     return Minute(
         start=start,
-        stop=Moment.from_datetime(stop),
+        stop=stop,
     )
 
 
@@ -41,14 +41,50 @@ _minute = minute
 
 def hour(year: int, month: int, day: int, hour: int) -> Hour:
     start = Moment(year, month, day, hour, 0, 0)
-    stop = start.to_datetime() + datetime.timedelta(hours=1)
+    stop = start + Delta(hours=1)
     return Hour(
         start=start,
-        stop=Moment.from_datetime(stop),
+        stop=stop,
     )
 
 
 _hour = hour
+
+
+def day(year: int, month: int, day: int) -> Day:
+    start = Moment(year, month, day, 0, 0, 0)
+    stop = start + Delta(days=1)
+    return Day(
+        start=start,
+        stop=stop,
+    )
+
+
+_day = day
+
+
+def month(year: int, month: int) -> Month:
+    start = Moment(year, month, 1, 0, 0, 0)
+    stop = start + Delta(months=1)
+    return Month(
+        start=start,
+        stop=stop,
+    )
+
+
+_month = month
+
+
+def year(year: int) -> Year:
+    start = Moment(year, 1, 1, 0, 0, 0)
+    stop = start + Delta(years=1)
+    return Year(
+        start=start,
+        stop=stop,
+    )
+
+
+_year = year
 
 
 @dataclasses.dataclass(frozen=True)
@@ -121,8 +157,105 @@ class Hour(Interval):
             yield minute
             minute = next(minute)
 
-    def __next__(self) -> Interval:
+    def minute(self, minute: int) -> Minute:
+        return _minute(
+            self.start.year,
+            self.start.month,
+            self.start.day,
+            self.start.hour,
+            minute,
+        )
+
+    def day(self) -> Day:
+        return _day(
+            self.start.year,
+            self.start.month,
+            self.start.day,
+        )
+
+    def __next__(self) -> Hour:
         return Hour(
             start=self.stop,
             stop=self.stop + Delta(hours=1),
+        )
+
+
+@dataclasses.dataclass(frozen=True)
+class Day(Interval):
+
+    def hours(self) -> Iterable[Hour]:
+        hour = _hour(
+            self.start.year,
+            self.start.month,
+            self.start.day,
+            0,
+        )
+        for _ in range(24):
+            yield hour
+            hour = next(hour)
+
+    def hour(self, hour: int) -> Hour:
+        return _hour(
+            self.start.year,
+            self.start.month,
+            self.start.day,
+            hour,
+        )
+
+    def __next__(self) -> Day:
+        return Day(
+            start=self.stop,
+            stop=self.stop + Delta(days=1),
+        )
+
+
+@dataclasses.dataclass(frozen=True)
+class Month(Interval):
+
+    def days(self) -> Iterable[Day]:
+        day = _day(
+            self.start.year,
+            self.start.month,
+            1,
+        )
+        for _ in range(31):
+            yield day
+            day = next(day)
+
+    def day(self, day: int) -> Day:
+        return _day(
+            self.start.year,
+            self.start.month,
+            day,
+        )
+
+    def __next__(self) -> Month:
+        return Month(
+            start=self.stop,
+            stop=self.stop + Delta(months=1),
+        )
+
+
+@dataclasses.dataclass(frozen=True)
+class Year(Interval):
+
+    def months(self) -> Iterable[Month]:
+        month = _month(
+            self.start.year,
+            1,
+        )
+        for _ in range(12):
+            yield month
+            month = next(month)
+
+    def month(self, month: int) -> Month:
+        return _month(
+            self.start.year,
+            month,
+        )
+
+    def __next__(self) -> Year:
+        return Year(
+            start=self.stop,
+            stop=self.stop + Delta(years=1),
         )
